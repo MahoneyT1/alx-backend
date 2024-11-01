0639168af71b15cd33e4d2c3720c36f514b9eff5
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-01 11:09:19 +0100

    applied lru algorithm

--- 0x01-caching/c.py
+++ 0x01-caching/c.py
@@ -1,9 +1,14 @@
-data = {
-    "name": "hary",
-    "age": 46,
-    "height": 1.67
-}
+# data = {
+#     "name": "hary",
+#     "age": 46,
+#     "height": 1.67
+# }
 
-data.pop("name")
-first_item = next(iter(data))
-print(first_item)
+# data.pop("name")
+# first_item = next(iter(data))
+# print(first_item)
+
+li = ['one', 'two', 'three']
+
+li.remove('one')
+print(li)
