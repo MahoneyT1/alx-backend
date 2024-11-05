dd146b81989704d36e34ea97613435b0696d799d
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-05 15:14:49 +0100

    implemented class Config and default tine zones

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+""" Then instantiate the Babel object in your app. Store it in
+a module-level variable named babel.
+
+In order to configure available languages in our app, you will
+create a Config class that has a LANGUAGES class attribute equal
+to ["en", "fr"].
+
+Use Config to set Babel’s default locale ("en") and timezone ("UTC").
+
+Use that class as config for your Flask app.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__, template_folder="templates")
+
+
+class Config:
+    """Flask config class"""
+
+    LANGUAGES = ["en", "fr"]
+    DEFAULT_LOCALE = 'en'
+    DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """ home route"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
