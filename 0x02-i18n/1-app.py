5aab65fd94e2d2641b0c0977d49287daabb12338
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-05 15:30:55 +0100

    runnina test

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -18,7 +18,13 @@ app = Flask(__name__, template_folder="templates")
 
 
 class Config:
-    """Flask config class"""
+    """Flask config class. Config class that has a
+    LANGUAGES class attribute equal to ["en", "fr"].
+    
+    Use Config to set Babel’s default locale ("en")
+    and timezone ("UTC"). Use that class as config for
+    your Flask app.
+    """
 
     LANGUAGES = ["en", "fr"]
     DEFAULT_LOCALE = "en"
@@ -31,9 +37,11 @@ babel = Babel(app)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
-    """ home route"""
+    """ home route that handles a get request on the hompage
+    also renders the template
+    """
     return render_template("1-index.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
