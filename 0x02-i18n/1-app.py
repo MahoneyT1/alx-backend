d9f614ec8462924b3336ee3d45ffab4351e71c2b
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-06 07:24:52 +0100

    fixed a code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -25,7 +25,7 @@ class Config:
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = pytz.utc
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
