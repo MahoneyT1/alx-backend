1704c68b835db1646f595c5ec6739dd2dd9090b1
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-06 03:30:20 +0100

    runnina test

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" Then instantiate the Babel object in your app. Store it in
+a module-level variable named babel.
+
+In order to configure available languages in our app, you will
+create a Config class that has a LANGUAGES class attribute equal
+to ["en", "fr"].
+
+Use Config to set Babel’s default locale ("en") and timezone ("UTC").
+
+Use that class as config for your Flask app.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__, template_folder="templates")
+
+
+class Config:
+    """Flask config class. Config class that has a
+    LANGUAGES class attribute equal to ["en", "fr"].
+    """
+
+    LANGUAGES = ["en", "fr"]
+    DEFAULT_LOCALE = "en"
+    DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+# Create a get_locale function with the babel.localeselector
+# decorator. Use request.accept_languages to determine the best
+# match with our supported languages.
+
+
+@babel.localeselector
+def get_local():
+    """Use request.accept_languages to determine the best
+    match with our supported languages.
+    """
+
+    return request
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """ home route that handles a get request on the hompage
+    also renders the template
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
