6dc4081e95ccbfded2a5f4b8635e90896eeee060
MahoneyT1 <ifeanyitech15@gmail.com>
2025-01-27 21:54:16 +0100

    implemented process queue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,25 +1,23 @@
 import kue from "kue";
 
-const queue = kue.createQueue()
 
 const jobData = {
     phoneNumber: "08131325356",
-    message: "Hello world"
+    message: "This is the code to verify your account"
 }
 
 const push_notification_code = kue.createQueue()
 
 const job = push_notification_code.create('notification', jobData)
-.save((err)=> {
-    if (err) {
-        console.error(`Notification job failed ${err}`)
-    }
-    else {
-        console.log(`Notification job created: ${job.id}`)
-    }
+.save(()=> {
+    console.log(`Notification job created: ${job.id}`)
 })
 
-push_notification_code.process("complete", (job, done)=> {
-    console.log(`Notification job completed ${job.data}`)
-    done();
+
+job.on("complete", ()=> {
+    console.log(`Notification job completed: ${job.id}`)
 });
+
+job.on("failed", ()=> {
+    console.log('Notification job failed')
+})
